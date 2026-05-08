024a67304b88e8d913a6780ef189370e1b28460f
Delta student <162330394+rajatR25@users.noreply.github.com>
2026-05-08 09:54:20 +0530

    update HR logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -228,7 +228,10 @@ const inviteHR = async (req, res) => {
 
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: 'smtp.gmail.com', 
+      port: 465,
+      secure: true,
+      family: 4,              
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
